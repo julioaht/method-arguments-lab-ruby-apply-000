48f53e68a857c573dfef17b110f6c0511078c4c4
julio hernandez <julioandres72@gmail.com>
2020-08-27 12:59:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,3 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-def introduction_with_language(name = "Dan", language = "Ember.js")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduduction_with_languaje (name, languaje)
+  puts "Hi, my name is #{name} and I am learning to program in #{languaje}"
 end
